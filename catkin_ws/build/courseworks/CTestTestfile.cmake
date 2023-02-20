c70fee8ee1929336ac86fe8255acf1fa7c9feb97
armavi-black <75437896+armavi-black@users.noreply.github.com>
2023-02-20 12:09:17 -0600

    Delete catkin_ws directory

--- catkin_ws/build/courseworks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/armando/catkin_ws/src/courseworks
-# Build directory: /home/armando/catkin_ws/build/courseworks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
